cb12005f32d5cc5dc633569b3fcbab0ff3341b62
bot-paulina <bot-paulina@DESKTOP-BUMICC3.>
2025-02-01 00:42:11 +0100

    added process_dataframe function to signal_processing.py, created main.py file

--- src/signal_processing.py
+++ src/signal_processing.py
@@ -1,6 +1,6 @@
-from scipy.signal import butter, filtfilt, iirnotch
+from scipy.signal import butter, filtfilt
 
-def bandpass_filter(data, lowcut, highcut, fs):
+def bandpass_filter(data, lowcut, highcut, fs, order=4):
     """
     Apply bandpass filter.
 
@@ -14,6 +14,8 @@ def bandpass_filter(data, lowcut, highcut, fs):
         Lower cutoff frequency.
     fs : int
         The sampling frequency.
+    order : int
+        The order of the filter.
 
     Returns
     -------
@@ -23,7 +25,7 @@ def bandpass_filter(data, lowcut, highcut, fs):
     nyquist_rate = 0.5 * fs
     lowcut = lowcut / nyquist_rate
     highcut = highcut / nyquist_rate
-    numerator, denominator = butter(order, [lowcut, highcut], btype='band')
+    numerator, denominator = butter(order, [lowcut, highcut], fs=fs, btype='band')
     return filtfilt(numerator, denominator, data)
 
 def compute_psd(data, fs):
@@ -61,3 +63,30 @@ def whiten_signal(data, fs):
         Description.
     """
     pass
+
+
+def process_dataframe(df, fs):
+    """
+    Apply all signal processing functions to the dataframe.
+
+    Parameters:
+    - df: Pandas DataFrame with 'time' and 'strain' columns
+    - fs: Sampling frequency (Hz)
+
+    Returns:
+    - Processed DataFrame
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Data frame with 'time' and 'strain' columns.
+    fs : int
+        The sampling frequency.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        Data frame with added column corresponding to processed data.
+    """
+    df["processed"] = bandpass_filter(df["strain"].values, fs)
+
+    return df
